f7fc464c314e372f92e486b2382c58bd438e837d
Daniel Eichhorn <dani.eichhorn@squix.ch>
2016-06-13 20:44:20 +0200

    Disabled resetting of wifi manager

--- src/main.cpp
+++ src/main.cpp
@@ -139,7 +139,8 @@ void setup() {
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wifiManager;
-  wifiManager.resetSettings();
+  // Uncomment for testing wifi manager
+  //wifiManager.resetSettings();
   wifiManager.setAPCallback(configModeCallback);
 
   //or use this for auto generated name ESP + ChipID
